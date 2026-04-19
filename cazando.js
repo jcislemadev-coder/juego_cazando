55cf977a3722a7f4e189f76a1283b8881418c559
jcislemadev-coder <jcislemadev@gmail.com>
2026-04-18 20:26:49 -0500

    inclusion de deteccion de Colision

--- cazando.js
+++ cazando.js
@@ -1,10 +1,10 @@
 let canvas = document.getElementById("areaJuego");
 let ctx= canvas.getContext("2d");
 
-const ALTO_GATO=70;
-const ANCHO_GATO=90;
-const ALTO_COMIDA=80;
-const ANCHO_COMIDA=40;
+const ALTO_GATO= 70;
+const ANCHO_GATO= 90;
+const ALTO_COMIDA=70;
+const ANCHO_COMIDA=90;
 
 let gatoX = (canvas.width/2)-(ANCHO_GATO/2);
 let gatoY= (canvas.height/2)-(ALTO_GATO/2);
@@ -38,20 +38,24 @@ function limpiarCanva(){
 function moverIzquierda(){
     gatoX= gatoX-10;
     actualizarPantalla();
+    detectarColision();
 }
 
 function moverDerecha(){
     gatoX= gatoX+10;
     actualizarPantalla();
+    detectarColision();
 }
 function moverArriba(){
     gatoY= gatoY-10;
     actualizarPantalla();
+    detectarColision();
 }
 
 function moverAbajo(){
     gatoY= gatoY+10;
     actualizarPantalla();
+    detectarColision();
 }
 function actualizarPantalla(){
     limpiarCanva();
@@ -59,3 +63,11 @@ function actualizarPantalla(){
     graficarComida();
 }
 
+function detectarColision(){
+    if(gatoX + ANCHO_GATO > comidaX &&
+       gatoX < comidaX + ANCHO_COMIDA &&
+       gatoY + ALTO_GATO > comidaY &&
+       gatoY <= comidaY + ALTO_COMIDA){
+    alert("GATO COMIENDO");
+    }
+}
